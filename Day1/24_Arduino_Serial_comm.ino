9304cd783ac636047d687d67ba7af451dbcf1272
goodlucky-Joy <goodlucky99@gmail.com>
2020-09-12 22:43:09 +0900

    Update and rename 19_Arduino_Serial_comm.ino to 24_Arduino_Serial_comm.ino

--- Day1/24_Arduino_Serial_comm.ino
+++ Day1/24_Arduino_Serial_comm.ino
@@ -1,3 +1,5 @@
+/* 아두이노 직렬통신을 통해 키보드로 부터 입력받은 값에 따라 LED 제어 예제 프로그램 */
+
 void setup(){
   Serial.begin(9600);
   pinMode(13,OUTPUT);
@@ -6,7 +8,7 @@ void setup(){
 void loop(){
   char c;
   if(Serial.available() > 0){  	// 수신 버퍼에 저장된 바이트 수 반환
-    c = Serial.read();	   	// 수신 버퍼에서 한 바이트 읽어옴
+    c = Serial.read();	   	    // 수신 버퍼에서 한 바이트 읽어옴
     if(c=='a'){
       digitalWrite(13, HIGH);
     }else{
