146ff5783d589e48d80c9ea3834cdb449bfe3385
goodlucky-Joy <goodlucky99@gmail.com>
2020-09-12 23:06:47 +0900

    Update 02_ClassLed_test.ino

--- Day3/02_ClassLed_test.ino
+++ Day3/02_ClassLed_test.ino
@@ -1,8 +1,9 @@
-//ClassLed_test.ino
-//LED On/Off 클래스 사용
+/* Led.h와 Led.cpp 파일에 Led클래스를 만든 후 객체를 생성하여 사용해보는 예제 프로그램 */
+/* Led.h와 Led.cpp는 내문서/Arduino/Libraries/Led 폴더를 만들어서 그 안에 넣어주세요 */
+
 #include <Led.h>
 
-Led obj(13);	//객체 선언
+Led obj(13);	//Led 클래스의 객체 선언
 
 void setup(){
 }
