5a0ffa151f57786b4417b70f95d705714a7283e9
goodlucky-Joy <goodlucky99@gmail.com>
2020-09-12 22:37:51 +0900

    Update 03_StandardIO_00_SwapValue.c

--- Day1/03_StandardIO_00_SwapValue.c
+++ Day1/03_StandardIO_00_SwapValue.c
@@ -1,10 +1,12 @@
+/* 두개의 변수의 값을 서로 바꿔주는 예제 프로그램 */
+
 #include<stdio.h>
 
 int main()
 {
     int x = 10;
     int y = 20;
-    int temp;
+    int temp;       //임시로 변수값을 저장할 공간
 
 
     printf("x = %d , y = %d\n", x,y);
