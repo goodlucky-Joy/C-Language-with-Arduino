2d63ad6e28da8f26dc544e31643e019b2fbd3995
goodlucky-Joy <goodlucky99@gmail.com>
2020-09-20 15:36:24 +0900

    Update 11_for_MultiTable02.c

--- Day2/11_for_MultiTable02.c
+++ Day2/11_for_MultiTable02.c
@@ -5,10 +5,11 @@ int main(void)
 {
 	int i, j, n;
 
-	for(i = 2;i <= 9; i++)
-        for(j = 1;j <= 9; j++)
-            printf("%d * %d = %d\n", i, j, i * j);
-
+	for(i = 2;i <= 9; i++){
+		for(j = 1;j <= 9; j++)
+		    printf("%d * %d = %d\t", i, j, i * j);
+		printf("\n");
+	}
 	return 0;
 }
 
