1395397e3703663a315bcf80d1cd0919c4582c27
goodlucky-Joy <goodlucky99@gmail.com>
2020-10-26 15:31:32 +0900

    Update 11_Operator_02_Increment_02_CompoundInterest.c

--- Day1/11_Operator_02_Increment_02_CompoundInterest.c
+++ Day1/11_Operator_02_Increment_02_CompoundInterest.c
@@ -8,26 +8,27 @@ int main(void)
 	printf("투자금을 입력하시오(만원 단위): ");
 	scanf("%d", &money);
 
+	printf("1. %f만원입니다. \n",  money * 0.03);
+	printf("1. %d만원입니다. \n", (int)(money * 0.03) );
+
 	++year;
-	printf("1. %f만원입니다. \n",  money * 0.12);
-	printf("1. %d만원입니다. \n", (int)(money * 0.12) );
-	money += (int)(money * 0.12);
+	money += (int)(money * 0.03);
 	printf("%d년 뒤의 투자금은 %d만원입니다. \n", year, money);
 
 	++year;
-	money += (int)(money * 0.12);
+	money += (int)(money * 0.03);
 	printf("%d년 뒤의 투자금은 %d만원입니다. \n", year, money);
 
 	++year;
-	money += (int)(money * 0.12);
+	money += (int)(money * 0.03);
 	printf("%d년 뒤의 투자금은 %d만원입니다. \n", year, money);
 
 	++year;
-	money += (int)(money * 0.12);
+	money += (int)(money * 0.03);
 	printf("%d년 뒤의 투자금은 %d만원입니다. \n", year, money);
 
 	++year;
-	money += (int)(money * 0.12);
+	money += (int)(money * 0.03);
 	printf("%d년 뒤의 투자금은 %d만원입니다. \n", year, money);
 
 	return 0;
