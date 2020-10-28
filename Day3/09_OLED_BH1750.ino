36718b3c3d886e91a603137c89d7647020583f21
goodlucky-Joy <goodlucky99@gmail.com>
2020-10-28 17:01:46 +0900

    Update 09_OLED_BH1750.ino

--- Day3/09_OLED_BH1750.ino
+++ Day3/09_OLED_BH1750.ino
@@ -1,8 +1,10 @@
-/* BH1750 조도센서로 입력받은 조도값을 OLED 디스플레이에 출력하는 프로그램 */
+/* 128x64 OLED에 Hello World를 출력하는 프로그램 */
 
+#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <BH1750.h>
 
 #define OLED_RESET LED_BUILTIN  //4
 Adafruit_SSD1306 display(OLED_RESET);
@@ -11,37 +13,34 @@ Adafruit_SSD1306 display(OLED_RESET);
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-#include <BH1750.h>
 BH1750 lightMeter;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   Wire.begin();
   lightMeter.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  displayIlluminance();
-  display.display();
-}
-
-void displayIlluminance(){
-  //Delay to allow sensor to stablize
-  delay(1000);
-  
-  //Read Illuminance
-  float lux = lightMeter.readLightLevel();
-  Serial.print("Illuminance: ");
-  Serial.print(lux);
-  Serial.println(" lx");
 
   // Clear the buffer.
   display.clearDisplay();
   display.display();
-  
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.print("Illuminance: ");
+  display.println("Hello World!");
+  display.display();
+}
+
+void loop() {
+  delay(500);
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+
+  display.clearDisplay();
+  display.display();
+  display.setCursor(0,0);
   display.println(lux);
+  display.display();
 }
